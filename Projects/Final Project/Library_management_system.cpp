2cdbdce3c8c859f4d5918b19c7343cf0591c1e83
istiyaq13 <122169844+ShahriarIstiyaq@users.noreply.github.com>
2024-06-14 01:44:10 +0600

    Update Library_management_system.cpp

--- Projects/Final Project/Library_management_system.cpp
+++ Projects/Final Project/Library_management_system.cpp
@@ -44,6 +44,27 @@ void gotoxy(int x, int y)
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
 
+// For showing the tile of the project.
+void showTitle(string s)
+{
+    //-----------------------------------------------------------Titile----------------------------------------------------//
+    gotoxy(70, 4);                // Upper bar of Title.
+    for (int i = 1; i <= 35; i++) // 35 dots.
+        printf("%c", 254);
+
+    gotoxy(70, 5); // Title of project.
+    cout << "     " << s << "     ";
+
+    gotoxy(70, 5); // Left bar of Title.
+    printf("%c", 219);
+    gotoxy(104, 5); // Right bar of Title.
+    printf("%c", 219);
+
+    gotoxy(70, 6);                // Lower bar of Title.
+    for (int i = 1; i <= 35; i++) // 35 dots.
+        printf("%c", 254);
+}
+
 /// Main class of Library:(all functions are inside this)
 class library
 {
@@ -52,7 +73,7 @@ private:
     {
         int id; // Book id
         string name, author, publisher;
-        Book *next_add; // Next field for adding new books.
+        Book *next; // Next field for adding new books.
     };
 
 public:
@@ -60,7 +81,57 @@ public:
     void menu();
     void EXIT();
     void insert();
+    void search();
+    void Ask_before_operation(string s);
+    void update();
+    void Delete();
+    void sort(); // Selection sort.
+    void ShowAll();
+    void AboutUs();
 };
+
+/// Sort Function:
+void library ::sort()
+{
+    if (head == NULL || head->next == NULL)
+    {
+        return; // List is empty or has only one element.
+    }
+
+    // Temporary node to hold the sorted part
+    Book *sorted = NULL;
+
+    while (head != NULL)
+    {
+        // Find the minimum node in the unsorted part
+        Book **min = &head;
+        Book *current = head;
+
+        while (current->next != NULL)
+        {
+            if (current->next->id < (*min)->id)
+            {
+                min = &(current->next);
+            }
+            current = current->next;
+        }
+
+        // Move the minimum node to the sorted part
+        Book *minNode = *min;
+        *min = minNode->next; // Remove the minimum node from the unsorted part
+        minNode->next = sorted;
+        sorted = minNode;
+    }
+
+    // Set head to the new sorted list
+    while (sorted != NULL)
+    {
+        Book *temp = sorted;
+        sorted = sorted->next;
+        temp->next = head;
+        head = temp;
+    }
+}
 
 /// Exit Function:
 void library::EXIT()
@@ -132,7 +203,403 @@ void library::EXIT()
         }
     }
 }
+
+/// Insert Function:
+void library ::insert()
+{
+    system("cls");
+    showTitle("Insert Record.");
+
+    // Crearing new node:
+    Book *newNode = new Book;
+
+    gotoxy(53, 8);
+    cout << "Book ID : ";
+    cin >> newNode->id;
+
+    gotoxy(53, 10);
+    cout << "Name : ";
+    cin.ignore();
+    getline(cin, newNode->name);
+
+    gotoxy(53, 12);
+    cout << "Author Name : ";
+    getline(cin, newNode->author);
+
+    gotoxy(53, 14);
+    cout << "Publisher Name : ";
+    getline(cin, newNode->publisher);
+
+    newNode->next = NULL; // Initially every next field is null;
+
+    // Insertion Process:(Insertion sort)
+    if (head == NULL || head->id >= newNode->id) // Empty LL.
+    {
+        newNode->next = head;
+        head = newNode;
+    }
+
+    else
+    {
+        Book *current = head;
+
+        while (current->next != NULL && current->next->id < newNode->id) // Searching for insertion position.
+            current = current->next;
+
+        // Inserting to correct node:
+        newNode->next = current->next;
+        current->next = newNode;
+    }
+
+    gotoxy(53, 17);
+    cout << "New Book Inserted Successfully....!!";
+
+    _getch();
+}
+
+/// Show ALL Funtion:
+void library::ShowAll()
+{
+    system("cls");
+    showTitle("List Of All Books");
+    Book *ptr = head;
+
+    if (head == NULL)
+    {
+        gotoxy(53, 8);
+        cout << "There Are No Books In The Library!!";
+        return;
+    }
+
+    int y = 8; // Y coordinate.
+    while (ptr != NULL)
+    {
+        gotoxy(53, y);
+        cout << "Book ID : " << ptr->id;
+        gotoxy(53, y + 1);
+        cout << "Name : " << ptr->name;
+        gotoxy(53, y + 2);
+        cout << "Author Name : " << ptr->author;
+        gotoxy(53, y + 3);
+        cout << "Publisher Name : " << ptr->publisher;
+        y += 5;
+        ptr = ptr->next;
+    }
+
+    _getch();
+}
+
+/// Search Function:
+void library::search()
+{
+    system("cls");
+    showTitle("Search Record");
+
+    int tempId, found = false;
+    if (head == NULL)
+    {
+        gotoxy(53, 8);
+        cout << "There Are No Books In The Library!!";
+        return;
+    }
+
+    else
+    {
+        gotoxy(53, 8);
+        cout << "Book ID : ";
+        cin >> tempId;
+
+        // Search Operation:
+        Book *ptr = head;
+
+        while (ptr != NULL) // sorted search.
+        {
+
+            if (tempId > ptr->id)
+                ptr = ptr->next;
+
+            else if (tempId == ptr->id)
+            {
+
+                system("cls");
+                showTitle("Search Record");
+
+                gotoxy(53, 8);
+                cout << "Book ID : " << ptr->id;
+
+                gotoxy(53, 10);
+                cout << "Name : " << ptr->name;
+
+                gotoxy(53, 12);
+                cout << "Author Name : " << ptr->author;
+
+                gotoxy(53, 14);
+                cout << "Publisher Name : " << ptr->publisher;
+
+                found = true;
+                break;
+            }
+
+            else
+                break;
+        }
+
+        gotoxy(53, 14);
+        if (!found)
+            cout << "The Book Is Not In The Library...!!";
+
+        _getch();
+    }
+}
+
+/// Update Function:
+
+void library::Ask_before_operation(string s)
+{
+    int position = 1;
+    int keyPressed = 0;
+    while (keyPressed != 13)
+    {
+        gotoxy(70, 22);
+        cout << "Do You Want To " << s << " This Record?";
+
+        gotoxy(80, 24);
+        arrowHere(1, position);
+        cout << " YES.";
+
+        gotoxy(80, 26);
+        arrowHere(2, position);
+        cout << " NO.";
+
+        keyPressed = getch();
+
+        if (keyPressed == 80 && position != 2)
+            position++;
+
+        else if (keyPressed == 72 && position != 1)
+            position--;
+
+        else if (keyPressed == 80 && position == 2)
+            position = 1;
+
+        else if (keyPressed == 72 && position == 1)
+            position = 2;
+
+        else
+            position = position;
+
+        if (keyPressed == 13)
+        {
+            if (position == 1)
+            {
+                system("cls");
+                return; // This will return to the Update function and continue the updating operations.
+            }
+            else if (position == 2)
+            {
+                system("cls");
+                menu();
+                return;
+            }
+        }
+    }
+}
+void library::update()
+{
+    system("cls");
+    showTitle("Update Record");
+
+    //----------------------------------Searching for the desired book--------------------------------------------------------------------//
+    int tempId, found = false;
+    Book *loc = NULL; // Location of desired book.
+
+    if (head == NULL)
+    {
+        gotoxy(53, 8);
+        cout << "There Are No Books In The Library!!";
+        return;
+    }
+
+    else
+    {
+        gotoxy(53, 8);
+        cout << "Book ID : ";
+        cin >> tempId;
+
+        // Search Operation:
+        Book *ptr = head;
+
+        while (ptr != NULL) // sorted search.
+        {
+            if (tempId > ptr->id)
+                ptr = ptr->next;
+
+            else if (tempId == ptr->id)
+            {
+                system("cls");
+                showTitle("Update Record");
+
+                gotoxy(53, 8); // Existing records of Books.
+                cout << "Existing Record: ";
+                gotoxy(53, 9);
+                for (int i = 1; i <= 18; i++)
+                    printf("%c", 95);
+
+                gotoxy(53, 12);
+                cout << "Book ID : " << ptr->id;
+
+                gotoxy(53, 14);
+                cout << "Name : " << ptr->name;
+
+                gotoxy(53, 16);
+                cout << "Author Name : " << ptr->author;
+
+                gotoxy(53, 18);
+                cout << "Publisher Name : " << ptr->publisher;
+
+                found = true;
+
+                loc = ptr; // Location of desired book.
+                break;
+            }
+
+            else
+                break;
+        }
+
+        if (found == false)
+        {
+            cout << "The Book Is Not In The Library...!!";
+            return;
+        }
+    }
+
+    //----------------------------------Searching for the desired book--------------------------------------------------------------------//
+
+    // --------------------------------Update operation:----------------------------------------------------------------------------------//
+
+    Ask_before_operation("Update");
+
+    system("cls");
+    showTitle("Update Record");
+
+    gotoxy(53, 8);
+    cout << "New Book ID: ";
+    cin >> loc->id;
+
+    gotoxy(53, 10);
+    cout << "New Name : ";
+    cin.ignore();
+    getline(cin, loc->name);
+
+    gotoxy(53, 12);
+    cout << "New Author Name : ";
+    getline(cin, loc->author);
+
+    gotoxy(53, 14);
+    cout << "New Publisher Name : ";
+    getline(cin, loc->publisher);
+
+    gotoxy(53, 16);
+    cout << "Updated Record Successfully...!!";
+
+    sort(); // sorting the data after operation.
+    _getch();
+}
+
+/// Delete Function:
+void library::Delete()
+{
+    system("cls");
+    showTitle("Delete Record");
+
+    int tempId;
+    bool found = false;
+    Book *prev = NULL; // For storing infromation of the previous node.
+
+    if (head == NULL)
+    {
+        gotoxy(53, 8);
+        cout << "There Are No Books In The Library!!";
+        _getch();
+        return;
+    }
+
+    else
+    {
+        gotoxy(53, 8);
+        cout << "Book ID : ";
+        cin >> tempId;
+
+        // Search Operation:
+        Book *ptr = head;
+
+        while (ptr != NULL)
+        {
+            if (tempId == ptr->id)
+            {
+                system("cls");
+                showTitle("Delete Record");
+
+                gotoxy(53, 8); // Existing records of Books.
+                cout << "Existing Record: ";
+                gotoxy(53, 9);
+                for (int i = 1; i <= 18; i++)
+                    printf("%c", 95);
+
+                gotoxy(53, 12);
+                cout << "Book ID : " << ptr->id;
+
+                gotoxy(53, 14);
+                cout << "Name : " << ptr->name;
+
+                gotoxy(53, 16);
+                cout << "Author Name : " << ptr->author;
+
+                gotoxy(53, 18);
+                cout << "Publisher Name : " << ptr->publisher;
+
+                found = true;
+
+                break;
+            }
+            else if (tempId > ptr->id)
+            {
+                prev = ptr;
+                ptr = ptr->next;
+            }
+        }
+
+        // Delete operation:
+        Ask_before_operation("Delete");
+
+        if (found)
+        {
+
+            if (prev == NULL) // First node.
+                head = ptr->next;
+
+            else
+                prev->next = ptr->next;
+
+            delete ptr;
+            gotoxy(53, 10);
+            cout << "Book Deleted Successfully...!!";
+        }
+
+        else if (!found)
+        {
+            gotoxy(53, 10);
+            cout << "The Book Is Not In The Library...!!";
+        }
+    }
+
+    sort(); // sorting the data after operation.
+    _getch();
+}
+
 /// Menu Function:
+
 void library::menu()
 {
     system("cls");
@@ -144,24 +611,7 @@ void library::menu()
     while (keyPressed != 13) // Ascii 13 is the code for "Enter key".
     {
         /// Menu interface:
-
-        //-----------------------------------------------------------Titile----------------------------------------------------//
-        gotoxy(70, 4);                // Upper bar of Title.
-        for (int i = 1; i <= 35; i++) // 35 dots.
-            printf("%c", 254);
-
-        gotoxy(70, 5); // Title of project.
-        cout << "     LIBRARY MANAGEMENT SYSTEM     ";
-
-        gotoxy(70, 5); // Left bar of Title.
-        printf("%c", 219);
-        gotoxy(104, 5); // Right bar of Title.
-        printf("%c", 219);
-
-        gotoxy(70, 6);                // Lower bar of Title.
-        for (int i = 1; i <= 35; i++) // 35 dots.
-            printf("%c", 254);
-
+        showTitle("LIBRARY MANAGEMENT SYSTEM");
         //---------------------------------------------Box Of Menu-----------------------------------------------------------------//
 
         gotoxy(50, 10);               // Upper bar of MENU
@@ -196,7 +646,7 @@ void library::menu()
 
         gotoxy(53, 17);
         arrowHere(1, position);
-        printf("1. Insert Record.");
+        printf("1. Insert New Record.");
 
         gotoxy(53, 19);
         arrowHere(2, position);
@@ -246,30 +696,37 @@ void library::menu()
             hideCursor();
             switch (position)
             {
-            case 1:
+            case 1: // Insert.
                 system("cls");
                 hideCursor();
-                cout << "Insert function called.";
+                insert();
                 break;
-            case 2:
+            case 2: // Search.
                 system("cls");
                 hideCursor();
-                cout << "search function called.";
+                // load_data();
+                sort(); // Sorting all data.
+                search();
                 break;
-            case 3:
+            case 3: // Update.
+
                 system("cls");
                 hideCursor();
-                cout << "Update function called.";
+                // load_data();
+                sort(); // Sorting all data.
+                update();
                 break;
-            case 4:
+            case 4: // delete.
                 system("cls");
                 hideCursor();
-                cout << "Delete function called.";
+                sort(); // Sorting all data.
+                Delete();
                 break;
             case 5:
                 system("cls");
                 hideCursor();
-                cout << "Show all function called.";
+                sort(); // Sorting all data.
+                ShowAll();
                 break;
             case 6:
                 system("cls");
@@ -280,13 +737,15 @@ void library::menu()
                 system("cls");
                 hideCursor();
                 EXIT();
-                cout << "exit function called.";
 
                 if (exitFlag == 1)
                     return;
+
+                break;
             }
         }
     }
+    menu();
 }
 
 int main()
